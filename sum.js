912a3181028766e59f1d36e08a383b4e75edab88
David Justice <davidjustice28@gmail.com>
2022-09-30 11:35:16 -0400

    install jest, write 4 tests for 4 functions

--- sum.js
+++ sum.js
@@ -1,5 +1,84 @@
-function Sum(a,b) {
+function add(a,b) {
     return a + b
 }
 
-module.exports = Sum
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+
+}
+
+
+function reverseString(str) {
+   let array = str.split("")
+   array.reverse()
+   let newWord = array.join("")
+   return newWord
+}
+
+const calculator = {
+    add:(a,b) => {
+        return a + b
+    },
+    sub: (a,b) => {
+        return a - b
+    },
+    multiply: (a,b) => {
+        return a * b
+    },
+    divide: (a,b) => {
+        return a / b
+    }
+}
+
+
+function findMin(array) {
+   let x = 0
+   array.forEach(element => {
+    if(element < x) x = element
+   })
+   return x
+     
+}
+
+function findMax(array) {
+    let x = 0
+    array.forEach(element => {
+     if(element > x) x = element
+    })
+    return x
+      
+ }
+
+ function findAvg(array) {
+    let x = null
+    array.forEach(element => {
+     x+= element
+    })
+
+    return x/array.length
+      
+ }
+
+ function getLength(array) {
+    return array.length
+ }
+
+ const analyzeArray = (array) => {
+    return {
+        min: findMin(array),
+        max: findMax(array),
+        avg: findAvg(array),
+        length: getLength(array)
+    }
+ }
+ 
+
+
+module.exports.capitalize = capitalize
+module.exports.reverseString = reverseString
+module.exports.calculator = calculator
+module.exports.findMin = findMin
+module.exports.findMax = findMax
+module.exports.findAvg = findAvg
+module.exports.getLength = getLength
+module.exports.analyzeArray = analyzeArray
--- sum.test.js
+++ sum.test.js
@@ -1,10 +1,38 @@
+const sum = require("./sum")
+const calculator = sum.calculator
 
-const sum = require('./sum')
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(3)
+test('capitalize first letter of string', () => {
+    expect(sum.capitalize("text")).toBe("Text")
 })
 
-test('verifies if the sum is greater than 5', () => {
-    expect(sum(2,4)).toBeGreaterThan(5)
+test('reverse string', () => {
+    expect(sum.reverseString("david")).toBe("divad")
+})
+
+//tests for calculator object
+
+test('test calculator object functions ', () => {
+    expect(calculator.add(1,2)).toBe(3)
+    expect(calculator.sub(10,1)).toBe(9)
+    expect(calculator.multiply(3,4)).toBe(12)
+    expect(calculator.divide(6,2)).toBe(3)
+})
+
+
+//tests for step 4 analyze function
+test('get the minimum value of an array', () => {
+    expect(sum.findMin([0,2,4,6])).toEqual(0)
+})
+
+test('get the max value of an array', () => {
+    expect(sum.findMax([0,2,4,6])).toEqual(6)
+})
+
+test('get average value for an array', () =>  {
+    expect(sum.findAvg([0,2,4,6])).toBe(3)
+})
+
+test('get length of an array', () => {
+    expect(sum.getLength([0,2,4,6])).toBe(4)
 })
